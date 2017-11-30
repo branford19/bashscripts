0679c4aaeba4da101d6a202dc27162708e85f9ad
branford19 <branford19@students.bdc.nsw.edu.au>
2017-11-30 11:27:37 +1100

    Fixed script (Or broke it more, can't tell)
    Amended:
    - Fixed installation method, was showing no installation candidate before.
    - Added other miscellaneous things.

--- Docker/dockerinstallxenial.sh
+++ Docker/dockerinstallxenial.sh
@@ -2,6 +2,10 @@ clear
 echo ""
 sleep 1
 echo ""
+# This bit removes the old versions of Docker.
+sudo apt-get remove docker docker-engine docker.io > /dev/null
+
+# User friendly things
 sleep 1
 echo ""
 sleep 1
@@ -15,13 +19,35 @@ echo "If you are on the root account, please control-c this script, exit the pro
 sleep 3
 echo "Please input your superuser password whenever prompted."
 sleep 1
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > /dev/null
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > /dev/null
-apt-get update > /dev/null
-apt-get install -y docker-ce > /dev/null
+
+# Installing the stuff we need to install the stuff
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# Installing docker repository key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Adding docker repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Refreshing the apt cache
+apt-get update
+
+# Installing docker
+sudo apt-get install docker-ce
+
+# Extra stuff to make it more user friendly
 sleep 1
 echo "Ok, I want you to just make sure that docker is running correctly."
 sleep 3
+
+# Fixing the user stuff and checking the status of docker
 sudo systemctl status docker
 sudo usermod -aG docker ${USER}
 echo "You'll need to enter your password here :)"
